5f9bd2f867f149bb8b26e247d06188cf6760b8c2
Ali Balash <86704540+AliBalash@users.noreply.github.com>
2026-06-05 20:08:31 +0330

    chore: make MongoDB connection configurable

--- src/database.js
+++ src/database.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 mongoose.set('strictQuery', false);
 
-mongoose.connect('mongodb://127.0.0.1:27017/prj5' )
+mongoose.connect(process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/prj5')
 .then(() => console.log('Connect To Mongoodb')).catch(() => console.log('Not Connect To Mongodb '))
 
